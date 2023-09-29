829980c91efe6ddaaea5d97eb09e79edc3630870
diegoG82 <diegogiardina@hotmail.it>
2023-09-29 17:57:39 +0200

    Messa UserIndex come pagina di avvio

--- la-mia-pizzeria-static/Program.cs
+++ la-mia-pizzeria-static/Program.cs
@@ -22,6 +22,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Pizza}/{action=Index}/{id?}");
+    pattern: "{controller=Pizza}/{action=UserIndex}/{id?}");
 
 app.Run();
