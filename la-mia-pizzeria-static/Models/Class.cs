149fdf2dcb51bdaadf76f6ad195e19d036930ae7
diegoG82 <diegogiardina@hotmail.it>
2023-09-29 12:27:50 +0200

    Creata classe Pizza

---
+++ la-mia-pizzeria-static/Models/Class.cs
@@ -0,0 +1,18 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Image {  get; set; }
+
+        public Pizza(string name, string description, string image )
+        {
+            this.Name = name;   
+            this.Description = description; 
+            this.Image = image;
+        }
+
+    }
+}
