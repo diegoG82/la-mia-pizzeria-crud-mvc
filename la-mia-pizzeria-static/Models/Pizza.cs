b82bed8ea0274129e9959dd7c38ee378e33a96ee
diegoG82 <diegogiardina@hotmail.it>
2023-10-02 15:19:10 +0200

    Fatto controllo bonus sulla lunghezza della descrizione con creazione della classe che estende la validazione

--- la-mia-pizzeria-static/Models/Pizza.cs
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -1,13 +1,24 @@
-﻿
+﻿using la_mia_pizzeria_static.ValidationAttributes;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace la_mia_pizzeria_static.Models
 {
     public class Pizza
-    {
+    {                               
         public int Id { get; set; }
+        [Required(ErrorMessage ="Name required")]
+        [MaxLength(100, ErrorMessage = "Max length of title is 100 characters")]
         public string Name { get; set; }
+        [Required(ErrorMessage = "Description required")]
+        [MoreThanFive]
         public string Description { get; set; }
+
+        [Url(ErrorMessage = "You must insert a valid link")]
+        [DefaultValue("../img/default.jpg")]
         public string Image {  get; set; }
 
+        [Range(0, 100, ErrorMessage = "Invalid price")]
         public int Price { get; set; }
 
         public Pizza() { }
---
+++ la-mia-pizzeria-static/ValidationAttributes/MoreThanFive.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace la_mia_pizzeria_static.ValidationAttributes
+{
+    public class MoreThanFive : ValidationAttribute
+    {
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value is string)
+            {
+                string inputValue = (string)value;
+
+                if (inputValue == null || inputValue.Split(' ').Length <= 5)
+                {
+                    return new ValidationResult("The field must contain more than five words!");
+                }
+                return ValidationResult.Success;
+
+            }
+
+            return new ValidationResult("The field is not a string");
+        }
+    }
+}
