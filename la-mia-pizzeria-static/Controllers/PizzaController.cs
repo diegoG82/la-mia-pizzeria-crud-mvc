1e284731df5e0dcb50381daa3d0972badad39a37
diegoG82 <diegogiardina@hotmail.it>
2023-10-03 13:07:29 +0200

    Crud update e delete implementate

--- la-mia-pizzeria-static/Controllers/PizzaController.cs
+++ la-mia-pizzeria-static/Controllers/PizzaController.cs
@@ -1,6 +1,7 @@
 ﻿using la_mia_pizzeria_static.Database;
 using la_mia_pizzeria_static.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
 
 namespace la_mia_pizzeria_static.Controllers
 {
@@ -38,7 +39,7 @@ namespace la_mia_pizzeria_static.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Pizza newPizza)
         {
-            if(newPizza.Image == null)
+            if (newPizza.Image == null)
             {
                 newPizza.Image = "/img/default.jpg";
             }
@@ -58,6 +59,57 @@ namespace la_mia_pizzeria_static.Controllers
             }
         }
 
+        [HttpGet]
+        public IActionResult Update(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+                Pizza? pizzaToEdit = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
+
+                if (pizzaToEdit == null)
+                {
+                    return NotFound("Pizza not founded");
+                }
+                else
+                {
+                    return View("Update", pizzaToEdit);
+                }
+            }
+        }
+
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Update(int id, Pizza modifiedPizza)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Update", modifiedPizza);
+            }
+
+            using (PizzaContext db = new PizzaContext())
+            {
+
+                Pizza? pizzaToUpdate = db.Pizzas.Find(id);
+
+                if (pizzaToUpdate != null)
+                {
+                    EntityEntry<Pizza> entryEntity = db.Entry(pizzaToUpdate);
+                    entryEntity.CurrentValues.SetValues(modifiedPizza);
+
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("No pizza to modify");
+                }
+
+
+            }
+
+        }
 
         public IActionResult Details(int id)
         {
@@ -76,7 +128,27 @@ namespace la_mia_pizzeria_static.Controllers
             }
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            using (PizzaContext db = new PizzaContext())
+            {
+              Pizza? pizzaToDelete = db.Pizzas.Where(pizza => pizza.Id == id).FirstOrDefault();
 
+                if (pizzaToDelete != null)
+                {
+                    db.Pizzas.Remove(pizzaToDelete);
+                    db.SaveChanges();
+
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    return NotFound("Pizza to delete not found!");
+                }
+            }
+        }
 
 
 
